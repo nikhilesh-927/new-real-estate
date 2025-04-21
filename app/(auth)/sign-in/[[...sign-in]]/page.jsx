0a0b895836c95484b23ed85e09eb5fa984aef16a
Nikhilesh Mishra <mishrajinikhil8@gmail.com>
2025-04-21 22:59:04 +0530

    new update logo change

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -29,12 +29,11 @@ export default function Page() {
         </a>
 
         <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-          Welcome to Homify 🏡
+          Welcome to HomeSathi 🏡
         </h2>
 
         <p className="mt-4 leading-relaxed text-white/90">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-          quibusdam aperiam voluptatum.
+        Every home has a story. Start yours with HomeSathi — the easiest way to explore, discover, and settle into the perfect space.
         </p>
       </div>
     </section>
@@ -63,12 +62,12 @@ export default function Page() {
           </a>
 
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Welcome to Homify 🏡
+            Welcome to HomeSathi 🏡
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
+            Every home has a story. Start yours with HomeSathi — the easiest way to explore, discover, and settle into the perfect space.
+       
           </p>
         </div>
 
